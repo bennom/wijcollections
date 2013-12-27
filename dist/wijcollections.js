10e140a6a8ce742dc86e83f4e6963f741fa91966
webinfluenza <look@webinfluenza.de>
2013-12-27 21:14:15 +0100

    clean up

--- dist/wijcollections.js
+++
@@ -1,9 +0,0 @@
-/*
- * wijcollections
- * https://github.com/webinfluenza/wijcollections
- *
- * Copyright (c) 2013 Benno Mielke
- * Licensed under the MIT license.
- */
-
-(function(e){require.config({baseUrl:"../",paths:{wijcModules:"lib/modules"}})})();
